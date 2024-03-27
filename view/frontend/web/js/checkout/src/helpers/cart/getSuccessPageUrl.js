f97cf2841b2f52ea3446215c3cfe5bab16df62bd
Alla Nehaenko <allanehaenko@gmail.com>
2024-03-27 14:52:19 +0000

    added fix for urls and prices format

--- view/frontend/web/js/checkout/src/helpers/cart/getSuccessPageUrl.js
+++ view/frontend/web/js/checkout/src/helpers/cart/getSuccessPageUrl.js
@@ -1,6 +1,6 @@
 import useConfigStore from '@/stores/ConfigStores/ConfigStore';
 
 export default () => {
-  const { secureBaseUrl, storeCode } = useConfigStore();
-  return `${secureBaseUrl}${storeCode}/checkout/onepage/success`;
+  const { secureBaseLinkUrl } = useConfigStore();
+  return `${secureBaseLinkUrl}checkout/onepage/success`;
 };
--- view/frontend/web/js/checkout/src/helpers/payment/formatPrice.js
+++ view/frontend/web/js/checkout/src/helpers/payment/formatPrice.js
@@ -2,5 +2,11 @@ import useConfigStore from '@/stores/ConfigStores/ConfigStore';
 
 export default (price) => {
   const { currencyCode: currency = 'GBP' } = useConfigStore();
-  return new Intl.NumberFormat('en-UK', { style: 'currency', currency }).format(price);
+
+  let locale = 'en-UK';
+  if (currency === 'USD') {
+    locale = 'en-US';
+  }
+
+  return new Intl.NumberFormat(locale, { style: 'currency', currency }).format(price);
 };
