f934f6ac683545c65d36518950d84acd8f9d1b5a
Alla Nehaenko <allanehaenko@gmail.com>
2024-03-27 17:21:53 +0000

    added fix for price formatter

--- view/frontend/web/js/checkout/src/helpers/payment/formatPrice.js
+++ view/frontend/web/js/checkout/src/helpers/payment/formatPrice.js
@@ -1,12 +1,7 @@
 import useConfigStore from '@/stores/ConfigStores/ConfigStore';
 
 export default (price) => {
-  const { currencyCode: currency = 'GBP' } = useConfigStore();
-
-  let locale = 'en-UK';
-  if (currency === 'USD') {
-    locale = 'en-US';
-  }
+  const { currencyCode: currency = 'GBP', locale } = useConfigStore();
 
   return new Intl.NumberFormat(locale, { style: 'currency', currency }).format(price);
 };
