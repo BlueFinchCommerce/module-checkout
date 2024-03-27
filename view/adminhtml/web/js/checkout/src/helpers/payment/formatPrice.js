fd1c8265c2fd3c9d8dfe05d467cef492dfbcc240
Alla Nehaenko <allanehaenko@gmail.com>
2024-03-27 17:31:51 +0000

    added fix for lint on admin

--- view/adminhtml/web/js/checkout/src/helpers/payment/formatPrice.js
+++ view/adminhtml/web/js/checkout/src/helpers/payment/formatPrice.js
@@ -2,6 +2,6 @@ import useConfigStore from '@/stores/ConfigStores/ConfigStore';
 
 export default (price) => {
   const { currencyCode: currency = 'GBP', locale } = useConfigStore();
-  
+
   return new Intl.NumberFormat(locale, { style: 'currency', currency }).format(price);
 };
