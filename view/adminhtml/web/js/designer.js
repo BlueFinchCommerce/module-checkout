2373e54d0a6f44a0c5dbe078435f8dc4a9d7737f
Dean Smart <dean.smart@gene.co.uk>
2024-03-27 15:38:31 +0000

    colour swatches added to inputs

--- view/adminhtml/web/js/designer.js
+++ view/adminhtml/web/js/designer.js
@@ -261,11 +261,31 @@ define([
         setupColorPickers: function () {
             const designerValuesArray = this.designerValues.val().split(';');
 
-            designerValuesArray.forEach((designerValue) => {
-                const [cssVariable, value] = designerValue.split(':'),
-                    input = this.designerModal.find(`[type="color"][data-css-variable="${cssVariable}]`);
+            this.designerModal.find('[data-type="color-picker"]').each((index, element) => {
+                const swatchId = $(element).data('css-variable') + '-swatch',
+                    defaultValue = $(element).attr('placeholder'),
+                    savedValue = designerValuesArray.find(function (item) {
+                        return item.split(':')[0] === $(element).data('css-variable');
+                    });
 
-                input.val(value);
+                // If saved value is found, set it as the element value
+                if (savedValue) {
+                    $('#' + swatchId).css('background-color', savedValue.split(':')[1]);
+                } else {
+                    // Set the placeholder as the default value
+                    $('#' + swatchId).css('background-color', defaultValue);
+                }
+
+                $(element).ColorPicker({
+                    onChange: function (hsb, hex) {
+                        $(element).val('#' + hex).trigger('change');
+                        $('#' + swatchId).css('background-color', '#' + hex);
+                    },
+
+                    onShow: function () {
+                        $(element).ColorPickerSetColor($(element).val());
+                    }
+                });
             });
         },
 
@@ -277,14 +297,10 @@ define([
                     confirm: function () {
                         this.designerModal.find('input').each(function (index, input) {
                             $(input).val('').trigger('change');
-                            // if (input.type === 'color') {
-                            //     $(input).val(input.placeholder).trigger('change');
-                            // } else {
-                            //     $(input).val('').trigger('change');
-                            // }
                         });
                         this.designerLogoDelete.trigger('click');
-                        // this.designerValues.val(' ').trigger('change');
+                        this.designerValues.val('').trigger('change');
+                        this.setupColorPickers();
                     }.bind(this)
                 }
             });
