88fe35bac217f39275ef88b4c7fd25b9e11d5bee
Alla Nehaenko <allanehaenko@gmail.com>
2024-08-13 16:06:09 +0100

    added alphabetical order for services index

--- view/frontend/web/js/checkout/src/services/index.js
+++ view/frontend/web/js/checkout/src/services/index.js
@@ -1,13 +1,13 @@
+import authenticatedRequest from '@/services/authenticatedRequest';
 import createPaymentRest from '@/services/payments/createPaymentRest';
 import getShippingMethods from '@/services/addresses/getShippingMethods';
 import getStoreConfig from '@/services/getStoreConfig';
 import refreshCustomerData from '@/services/customer/refreshCustomerData';
-import authenticatedRequest from '@/services/authenticatedRequest';
 
 export default {
+  authenticatedRequest,
   createPaymentRest,
   getShippingMethods,
   getStoreConfig,
   refreshCustomerData,
-  authenticatedRequest,
 };
