1252907dc718b916c81238fd48a4bf06a6cfb81c
Max Parker <max.parker@gene.co.uk>
2024-08-20 16:37:32 +0100

    Export saved delivery and shipping components

--- view/frontend/web/js/checkout/src/components/index.js
+++ view/frontend/web/js/checkout/src/components/index.js
@@ -5,6 +5,9 @@ import Price from '@/components/Core/ContentComponents/Price/Price.vue';
 import PrivacyPolicy from '@/components/Core/ContentComponents/PrivacyPolicy/PrivacyPolicy.vue';
 import RadioButton from '@/components/Core/ActionComponents/Inputs/RadioButton/RadioButton.vue';
 import Recaptcha from '@/components/Steps/PaymentPage/Recaptcha/Recaptcha.vue';
+import SavedDeliveryAddress
+  from '@/components/Steps/CustomerInfoPage/Addresses/SavedDeliveryAddess/SavedDeliveryAddess.vue';
+import SavedShippingMethod from '@/components/Steps/PaymentPage/SavedShippingMethod/SavedShippingMethod.vue';
 import TextField from '@/components/Core/ContentComponents/TextField/TextField.vue';
 import TextInput from '@/components/Core/ActionComponents/Inputs/TextInput/TextInput.vue';
 import Tick from '@/components/Core/Icons/Tick/Tick.vue';
@@ -17,6 +20,8 @@ export default {
   PrivacyPolicy,
   RadioButton,
   Recaptcha,
+  SavedDeliveryAddress,
+  SavedShippingMethod,
   TextField,
   TextInput,
   Tick,
