fcd4cd6107b427475ea96b93e5e9219f39cc5aa6
Alla Nehaenko <allanehaenko@gmail.com>
2024-08-29 17:55:22 +0100

    added fixes for ship-2-store module feedback

--- view/frontend/web/js/checkout/src/components/Steps/CustomerInfoPage/Addresses/AddressForms/BillingForm/BillingForm.vue
+++ view/frontend/web/js/checkout/src/components/Steps/CustomerInfoPage/Addresses/AddressForms/BillingForm/BillingForm.vue
@@ -98,7 +98,7 @@
         </div>
 
         <AddressForm
-          v-if="selected[address_type].editing || !addressFinder.enabled"
+          v-if="showFullBillingForm || selected[address_type].editing || !addressFinder.enabled"
           :address_type="address_type"
         />
       </div>
@@ -149,6 +149,10 @@ export default {
     showCheckbox: {
       type: Boolean,
       default: true,
+    },
+    showFullBillingForm: {
+      type: Boolean,
+      default: false,
     },
   },
   emits: [
