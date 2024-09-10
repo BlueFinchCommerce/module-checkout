6eaa43cbb733cb60a9198795302b8c9a711fd2cc
Alla Nehaenko <allanehaenko@gmail.com>
2024-09-10 16:52:40 +0100

    removed adyen from admin builder

--- view/adminhtml/web/js/checkout/src/helpers/storeConfigs/handleInitialConfig.js
+++ view/adminhtml/web/js/checkout/src/helpers/storeConfigs/handleInitialConfig.js
@@ -1,17 +1,14 @@
-import useAdyenStore from '@/stores/PaymentStores/AdyenStore';
 import useAgreementStore from '@/stores/ConfigStores/AgreementStore';
 import useBraintreeStore from '@/stores/PaymentStores/BraintreeStore';
 import useConfigStore from '@/stores/ConfigStores/ConfigStore';
 import useRecaptchaStore from '@/stores/ConfigStores/RecaptchaStore';
 
 export default async (data) => {
-  const adyenStore = useAdyenStore();
   const agreementStore = useAgreementStore();
   const braintreeStore = useBraintreeStore();
   const configStore = useConfigStore();
   const recaptchaStore = useRecaptchaStore();
 
-  adyenStore.handleInitialConfig(data);
   agreementStore.handleInitialConfig(data);
   braintreeStore.handleInitialConfig(data);
   configStore.handleInitialConfig(data);
