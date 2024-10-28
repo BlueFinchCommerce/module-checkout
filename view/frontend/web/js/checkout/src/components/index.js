25127db84a9050f3b64388c9f9cab6d1c7fe9c1f
Alla Nehaenko <allanehaenko@gmail.com>
2024-10-28 11:28:31 +0000

    added missing components/setvices/helpers to the index files

--- view/frontend/web/js/checkout/src/components/index.js
+++ view/frontend/web/js/checkout/src/components/index.js
@@ -1,59 +1,208 @@
 import AddressBlock from '@/components/Steps/CustomerInfoPage/Addresses/AddressBlock/AddressBlock.vue';
+import AddressBlockShort from '@/components/Steps/CustomerInfoPage/Addresses/AddressBlockShort/AddressBlockShort.vue';
+import AddressFinder from '@/components/Steps/CustomerInfoPage/AddressFinder/AddressFinder.vue';
+import AddressForms from '@/components/Steps/CustomerInfoPage/Addresses/AddressForms/Form/AddressForm.vue';
 import AddressList from '@/components/Steps/CustomerInfoPage/Addresses/AddressList/AddressList.vue';
+import AfdPostCode from '@/components/Steps/CustomerInfoPage/AddressFinder/AfdPostCode/AfdPostCode.vue';
 import Agreements from '@/components/Core/ContentComponents/Agreements/Agreements.vue';
+import ApplePay from '@/components/Steps/PaymentPage/Braintree/ApplePay/ApplePay.vue';
+import ArrowDown from '@/components/Core/Icons/ArrowDown/ArrowDown.vue';
+import ArrowLeft from '@/components/Core/Icons/ArrowLeft/ArrowLeft.vue';
+import ArrowRight from '@/components/Core/Icons/ArrowRight/ArrowRight.vue';
+import ArrowUp from '@/components/Core/Icons/ArrowUp/ArrowUp.vue';
+import BillingAddressIcon from '@/components/Core/Icons/BillingAddressIcon/BillingAddressIcon.vue';
 import BillingForm from '@/components/Steps/CustomerInfoPage/Addresses/AddressForms/BillingForm/BillingForm.vue';
+import BraintreeAch from '@/components/Steps/PaymentPage/Braintree/DropIn/BraintreeAch/BraintreeAch.vue';
+import BraintreeLpm from '@/components/Steps/PaymentPage/Braintree/DropIn/BraintreeLpm/BraintreeLpm.vue';
 import Calendar from '@/components/Core/ActionComponents/Calendar/Calendar.vue';
 import Checkbox from '@/components/Core/ActionComponents/Inputs/Checkbox/Checkbox.vue';
+import ClickCollectTabIcon from '@/components/Core/Icons/ClickCollectTabIcon/ClickCollectTabIcon.vue';
+import Clock from '@/components/Core/Icons/Clock/Clock.vue';
+import Close from '@/components/Core/Icons/Close/Close.vue';
+import Container from '@/components/Core/Container/Container.vue';
+import CouponDiscount from '@/components/Steps/GlobalComponents/OrderSummary/CouponDiscount/CouponDiscount.vue';
+import CouponIcon from '@/components/Core/Icons/CouponIcon/CouponIcon.vue';
+import DeliveryTabIcon from '@/components/Core/Icons/DeliveryTabIcon/DeliveryTabIcon.vue';
+import DetailsPage from '@/components/Steps/CustomerInfoPage/DetailsPage/DetailsPage.vue';
+import DividerComponent from '@/components/Steps/CustomerInfoPage/DividerComponent/DividerComponent.vue';
+import DropDown from '@/components/Core/ActionComponents/DropDown/DropDown.vue';
+import DropIn from '@/components/Steps/PaymentPage/Braintree/DropIn/DropIn.vue';
 import Edit from '@/components/Core/Icons/Edit/Edit.vue';
+import EmailAddress from '@/components/Steps/CustomerInfoPage/EmailAddress/EmailAddress.vue';
+import ErrorIcon from '@/components/Core/Icons/ErrorIcon/ErrorIcon.vue';
 import ErrorMessage from '@/components/Core/ContentComponents/Messages/ErrorMessage/ErrorMessage.vue';
+import Fixed from '@/components/Core/Fixed/Fixed.vue';
+import Footer from '@/components/Footer/Footer.vue';
+import FooterIcons from '@/components/Footer/FooterIcons/FooterIcons.vue';
+import FreeMOCheckPayment from '@/components/Steps/PaymentPage/FreeMOCheckPayment/FreeMOCheckPayment.vue';
+import GiftCardDiscount from '@/components/Steps/GlobalComponents/OrderSummary/GiftCardDiscount/GiftCardDiscount.vue';
+import GiftCardIcon from '@/components/Core/Icons/GiftCardIcon/GiftCardIcon.vue';
 import GoogleMap from '@/components/Steps/CustomerInfoPage/Addresses/GoogleMap/GoogleMap.vue';
+import GooglePay from '@/components/Steps/PaymentPage/Braintree/GooglePay/GooglePay.vue';
+import Header from '@/components/Header/Header.vue';
+import HideIcon from '@/components/Core/Icons/HideIcon/HideIcon.vue';
+import IframeComponent from '@/components/Core/ActionComponents/Iframe/Iframe.vue';
 import LinkComponent from '@/components/Core/ActionComponents/Link/Link.vue';
 import Loader from '@/components/Core/Icons/Loader/Loader.vue';
 import Locate from '@/components/Core/Icons/Locate/Locate.vue';
-import IframeComponent from '@/components/Core/ActionComponents/Iframe/Iframe.vue';
+import Lock from '@/components/Core/Icons/Lock/Lock.vue';
+import Logo from '@/components/Core/Icons/Logo/Logo.vue';
+import Loqate from '@/components/Steps/CustomerInfoPage/AddressFinder/Loqate/Loqate.vue';
 import Modal from '@/components/Core/ActionComponents/Modal/Modal.vue';
+import MinusIcon from '@/components/Core/Icons/MinusIcon/MinusIcon.vue';
 import MyButton from '@/components/Core/ActionComponents/Button/Button.vue';
 import NameFields from '@/components/Steps/CustomerInfoPage/Addresses/AddressForms/Form/Name/Name.vue';
+import NewMethods from '@/components/Steps/PaymentPage/Braintree/DropIn/NewMethods/NewMethods.vue';
+import Newsletter from '@/components/Core/ContentComponents/Newsletter/Newsletter.vue';
+import OrderSummaryDesktop
+  from '@/components/Steps/GlobalComponents/OrderSummary/OrderSummaryDesktop/OrderSummaryDesktop.vue';
+import OrderSummaryItem from '@/components/Steps/GlobalComponents/OrderSummary/OrderSummaryItem/OrderSummaryItem.vue';
+import OrderSummaryMobile
+  from '@/components/Steps/GlobalComponents/OrderSummary/OrderSummaryMobile/OrderSummaryMobile.vue';
+import OrderSummaryTitleWithAmount
+  from '@/components/Steps/GlobalComponents/OrderSummary/OrderSummaryTitleWithAmount/OrderSummaryTitleWithAmount.vue';
+import OrderSummaryTotal
+  from '@/components/Steps/GlobalComponents/OrderSummary/OrderSummaryTotal/OrderSummaryTotal.vue';
+import PayPal from '@/components/Steps/PaymentPage/Braintree/PayPal/PayPal.vue';
+import Payment from '@/components/Core/Icons/Payment/Payment.vue';
+import PaymentPage from '@/components/Steps/PaymentPage/PaymentPage.vue';
+import PayWithComponent from '@/components/Steps/GlobalComponents/PayWithComponent/PayWith.vue';
+import PenniesComponent from '@/components/Steps/PaymentPage/PenniesComponent/Pennies.vue';
+import Percent from '@/components/Core/Icons/Percent/Percent.vue';
+import PlusIcon from '@/components/Core/Icons/PlusIcon/PlusIcon.vue';
 import Price from '@/components/Core/ContentComponents/Price/Price.vue';
 import PrivacyPolicy from '@/components/Core/ContentComponents/PrivacyPolicy/PrivacyPolicy.vue';
+import ProgressBar from '@/components/Steps/GlobalComponents/ProgressBar/ProgressBar.vue';
+import ProgressBarArrow from '@/components/Core/Icons/ProgressBarArrow/ProgressBarArrow.vue';
+import PromotionComponent
+  from '@/components/Steps/GlobalComponents/OrderSummary/PromotionComponent/PromotionComponent.vue';
 import RadioButton from '@/components/Core/ActionComponents/Inputs/RadioButton/RadioButton.vue';
 import Recaptcha from '@/components/Steps/PaymentPage/Recaptcha/Recaptcha.vue';
-import Search from '@/components/Core/Icons/Search/Search.vue';
-import TextArea from '@/components/Core/ActionComponents/Inputs/TextArea/TextArea.vue';
+import Remove from '@/components/Core/Icons/Remove/Remove.vue';
+import Rewards from '@/components/Core/ContentComponents/Rewards/Rewards.vue';
 import SavedDeliveryAddress
   from '@/components/Steps/CustomerInfoPage/Addresses/SavedDeliveryAddess/SavedDeliveryAddess.vue';
 import SavedShippingMethod from '@/components/Steps/PaymentPage/SavedShippingMethod/SavedShippingMethod.vue';
+import Search from '@/components/Core/Icons/Search/Search.vue';
+import Select from '@/components/Core/ActionComponents/Inputs/Select/Select.vue';
+import Shipping from '@/components/Core/Icons/Shipping/Shipping.vue';
+import ShippingMethod from '@/components/Steps/ShippingPage/ShippingMethod/ShippingMethod.vue';
+import ShippingPage from '@/components/Steps/ShippingPage/ShippingPage.vue';
+import ShippingForm from '@/components/Steps/CustomerInfoPage/Addresses/AddressForms/ShippingForm/ShippingForm.vue';
+import ShowIcon from '@/components/Core/Icons/ShowIcon/ShowIcon.vue';
+import SlideUp from '@/components/Core/ActionComponents/SlideUp/SlideUp.vue';
+import StepCompletedIcon from '@/components/Core/Icons/StepCompletedIcon/StepCompletedIcon.vue';
+import StoreCredit from '@/components/Steps/PaymentPage/StoreCredit/StoreCredit.vue';
+import SuccessIcon from '@/components/Core/Icons/SuccessIcon/SuccessIcon.vue';
+import SuccessMessage from '@/components/Core/ContentComponents/Messages/SuccessMessage/SuccessMessage.vue';
+import TermsService from '@/components/Core/ContentComponents/TermsService/TermsService.vue';
+import TextArea from '@/components/Core/ActionComponents/Inputs/TextArea/TextArea.vue';
 import TextField from '@/components/Core/ContentComponents/TextField/TextField.vue';
 import TextInput from '@/components/Core/ActionComponents/Inputs/TextInput/TextInput.vue';
 import Tick from '@/components/Core/Icons/Tick/Tick.vue';
+import ValidIcon from '@/components/Core/Icons/ValidIcon/ValidIcon.vue';
+import VaultedMethods from '@/components/Steps/PaymentPage/Braintree/DropIn/VaultedMethods/VaultedMethods.vue';
+import YourDetails from '@/components/Core/Icons/YourDetails/YourDetails.vue';
 
 // Components
 export default {
   AddressBlock,
+  AddressBlockShort,
+  AddressFinder,
+  AddressForms,
   AddressList,
+  AfdPostCode,
   Agreements,
+  ApplePay,
+  ArrowDown,
+  ArrowLeft,
+  ArrowRight,
+  ArrowUp,
+  BillingAddressIcon,
   BillingForm,
+  BraintreeAch,
+  BraintreeLpm,
   Calendar,
   Checkbox,
+  ClickCollectTabIcon,
+  Clock,
+  Close,
+  Container,
+  CouponDiscount,
+  CouponIcon,
+  DeliveryTabIcon,
+  DetailsPage,
+  DividerComponent,
+  DropDown,
+  DropIn,
   Edit,
+  EmailAddress,
+  ErrorIcon,
   ErrorMessage,
+  Fixed,
+  Footer,
+  FooterIcons,
+  FreeMOCheckPayment,
+  GiftCardDiscount,
+  GiftCardIcon,
   GoogleMap,
+  GooglePay,
+  Header,
+  HideIcon,
+  IframeComponent,
   LinkComponent,
   Loader,
   Locate,
-  IframeComponent,
+  Lock,
+  Logo,
+  Loqate,
   Modal,
+  MinusIcon,
   MyButton,
   NameFields,
+  NewMethods,
+  Newsletter,
+  OrderSummaryDesktop,
+  OrderSummaryItem,
+  OrderSummaryMobile,
+  OrderSummaryTitleWithAmount,
+  OrderSummaryTotal,
+  PayPal,
+  Payment,
+  PaymentPage,
+  PayWithComponent,
+  PenniesComponent,
+  Percent,
+  PlusIcon,
   Price,
   PrivacyPolicy,
-  Search,
+  ProgressBar,
+  ProgressBarArrow,
+  PromotionComponent,
   RadioButton,
   Recaptcha,
-  TextArea,
+  Remove,
+  Rewards,
   SavedDeliveryAddress,
   SavedShippingMethod,
+  Search,
+  Select,
+  Shipping,
+  ShippingMethod,
+  ShippingPage,
+  ShippingForm,
+  ShowIcon,
+  SlideUp,
+  StepCompletedIcon,
+  StoreCredit,
+  SuccessIcon,
+  SuccessMessage,
+  TermsService,
+  TextArea,
   TextField,
   TextInput,
   Tick,
+  ValidIcon,
+  VaultedMethods,
+  YourDetails,
 };
