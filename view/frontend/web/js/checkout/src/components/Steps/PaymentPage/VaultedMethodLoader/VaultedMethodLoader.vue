fbff72d59b844619b90d0a52a927570d01ab93d3
Alla Nehaenko <allanehaenko@gmail.com>
2026-04-30 15:13:05 +0100

    added loader for vaulted and changed icon on saved payments

---
+++ view/frontend/web/js/checkout/src/components/Steps/PaymentPage/VaultedMethodLoader/VaultedMethodLoader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="vaulted-method-loader"
+    aria-hidden="true"
+  >
+    <div class="vaulted-method-loader__card text-loading" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.vaulted-method-loader {
+  margin-bottom: var(--indent__base);
+  width: 100%;
+
+  &__card {
+    min-height: var(--vaulted-method-loader-min-height, 138px);
+    border-radius: var(--selectable__border-radius, 8px);
+    max-width: var(--vaulted-method-loader-max-width, 440px);
+  }
+}
+</style>
