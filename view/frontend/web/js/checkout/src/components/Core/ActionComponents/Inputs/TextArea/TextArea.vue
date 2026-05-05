1d7ebd75664f9003e03fbea049d94880ecfbd197
Alla Nehaenko <allanehaenko@gmail.com>
2026-05-05 11:10:53 +0100

    trigger for ci

--- view/frontend/web/js/checkout/src/components/Core/ActionComponents/Inputs/TextArea/TextArea.vue
+++ view/frontend/web/js/checkout/src/components/Core/ActionComponents/Inputs/TextArea/TextArea.vue
@@ -178,7 +178,6 @@ export default {
 
       const debouncedResize = debounce(() => {
         const boundingRect = container.getBoundingClientRect();
-
         const isHidden = (window.innerHeight - footerHeight) < boundingRect.bottom;
 
         if (isHidden) {
